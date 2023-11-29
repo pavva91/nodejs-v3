eb332778b1f0fab511f38c7f96f44aa8ec1852c7
pavva91 <valerio.mattioli@proton.me>
2023-11-29 23:19:51 +0100

    index.js Pattern for modules folder

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+import * as thing from "./utils"
+
+import fs from "node:fs"; // explicit
+import _ from "lodash";
 
 const note = process.argv[2];
 const newNote = {
     content: note,
-    id: Date.now()
-}
+    id: Date.now(),
+};
 
-console.log(newNote)
+console.log(newNote);
---
+++ utils/index.js
@@ -0,0 +1,2 @@
+export { other } from "./other"
+export { count, somethingElse } from "./utils"
