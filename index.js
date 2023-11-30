d67088e8c5ee16e2359a31f00b6f827ad5accdde
pavva91 <valerio.mattioli@proton.me>
2023-11-30 02:38:42 +0100

    Notes App Commands

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
 #!/usr/bin/env node
-import * as thing from "./utils"
 
-import fs from "node:fs"; // explicit
-import _ from "lodash";
-
-const note = process.argv[2];
-const newNote = {
-    content: note,
-    id: Date.now(),
-};
-
-console.log(newNote);
+import './src/command.js'
